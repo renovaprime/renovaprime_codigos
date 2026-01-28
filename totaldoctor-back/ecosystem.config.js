985c26e1c2b80eb301bacc14f9aad585b2ddfe15
renovaprime <ramon@opemcode.com>
2026-01-28 18:50:27 +0000

    Update from https://github.com/git-opemcode/totalmedi-back-v2/commit/2f7a6083b6c95ef5336b1891529b7f96c5b1a797

--- totaldoctor-back/ecosystem.config.js
+++ totaldoctor-back/ecosystem.config.js
@@ -8,6 +8,11 @@ module.exports = {
     max_memory_restart: "400M",
     env: {
       NODE_ENV: "production"
-    }
-  }]
+    },
+    error_file: "./logs/pm2-error.log",
+    out_file: "./logs/pm2-out.log",
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    merge_logs: true,
+  }
+  ]
 };
